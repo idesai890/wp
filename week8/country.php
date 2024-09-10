0f8b57e6ab485e8654cac2c29563b0fa89e216c5
Tanya Unterberger <e102767@rmit.edu.au>
2024-09-10 16:55:47 +1000

    looping through records

--- week8/country.php
+++ week8/country.php
@@ -4,6 +4,11 @@ include_once 'includes/header.inc';
 <main>
     <?php
     require 'includes/db_connect.inc';
+    $result = $conn->query("SELECT * FROM country");
+    while ($row = $result->fetch_assoc()) {
+        echo "<h2>" . $row['countryid'] . ' ' . $row['countryname'] . "</h2>";
+        echo "<p>" . $row['description'] . "</p>";
+    }
     ?>
 </main>
 <?php
