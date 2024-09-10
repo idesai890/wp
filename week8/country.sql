6314ce69e15fb50e4e4541475ed1c295bde7fd0c
Tanya Unterberger <e102767@rmit.edu.au>
2024-09-11 08:57:11 +1000

    images

--- week8/country.sql
+++ week8/country.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 10, 2024 at 08:39 AM
+-- Generation Time: Sep 11, 2024 at 12:53 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,17 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `country` (
   `countryid` tinyint(4) NOT NULL,
   `countryname` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `caption` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `country`
 --
 
-INSERT INTO `country` (`countryid`, `countryname`, `description`) VALUES
-(1, 'Australia', 'The best country ever'),
-(3, 'Ukraine', 'Beautiful but war-torn place'),
-(5, 'China', 'A large and mighty country ');
+INSERT INTO `country` (`countryid`, `countryname`, `description`, `image`, `caption`) VALUES
+(1, 'Australia', 'The best country ever', 'images/australia.jpeg', 'Australia'),
+(3, 'Ukraine', 'Beautiful but war-torn place', 'images/kiev.jpeg', 'Kiev'),
+(5, 'China', 'A large and mighty country ', 'images/china.jpeg', 'China'),
+(6, 'USA', 'The United States of America (USA or U.S.A.), commonly known as the United States (US or U.S.) or America, is a country primarily located in North America. It is a federal union of 50 states, the federal capital district of Washington, D.C., and 326 Indian reservations.', 'images/usa.jpeg', 'Capitol');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +63,7 @@ ALTER TABLE `country`
 -- AUTO_INCREMENT for table `country`
 --
 ALTER TABLE `country`
-  MODIFY `countryid` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `countryid` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
