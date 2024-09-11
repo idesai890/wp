63269b56f32eb760954b4b4f3f22d1d0e48aab7f
Tanya Unterberger <e102767@rmit.edu.au>
2024-09-12 08:47:22 +1000

    reverted week8

--- week8/country.php
+++ week8/country.php
@@ -8,7 +8,6 @@ include_once 'includes/header.inc';
     while ($row = $result->fetch_assoc()) {
         echo "<h2>" . $row['countryid'] . ' ' . $row['countryname'] . "</h2>";
         echo "<p>" . $row['description'] . "</p>";
-        echo "<p><img src='" . $row['image'] . "' alt='" . $row['caption'] . "' width=300></p>";
     }
     ?>
 </main>
